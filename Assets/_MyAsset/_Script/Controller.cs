61296e15a5c1ebf0c1960116223c69d1863c4928
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-02 13:35:01 +0800

    first commit

---
+++ Assets/_MyAsset/_Script/Controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+	public Image Picture;
+	private GameObject Notice_1,Notice_2,Notice_3,Notice_3_DraggingObject,Notice_4;
+	public GameObject PictureCon;
+//	CameraUI
+	// Use this for initialization
+	void Start () {
+		Picture = CameraController.gPicture;
+		PictureCon.SetActive (true);
+		DraggingObjects.PimplesCount = 0;
+//		CameraUI = GameObject.Find("CameraUI").GetComponent<Image>();
+//		float alpha = 1.0f;
+//		CameraUI.color.a = alpha;
+
+		Notice_1 = GameObject.Find ("Notice_1");
+		Notice_2 = GameObject.Find ("Notice_2");
+		Notice_3 = GameObject.Find ("Notice_3");
+		Notice_3_DraggingObject = GameObject.Find ("Notice_3 _DraggingObject");
+		Notice_4 = GameObject.Find ("Notice_4");
+
+		Notice_1.SetActive (true);
+		Notice_2.SetActive (false);
+		Notice_3.SetActive (false);
+		Notice_3_DraggingObject.SetActive (false);
+		Notice_4.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(DraggingObjects.PimplesCount == 4){
+			FuncNotice_4 ();
+		}
+	}
+
+	public void Function(string FuncName){
+		if(FuncName == "Notice_2"){FuncNotice_2 ();}
+		if(FuncName == "Notice_3"){FuncNotice_3 ();}
+	}
+
+	void FuncNotice_2(){
+		Notice_1.SetActive (false);
+		Notice_2.SetActive (true);
+	}
+
+	void FuncNotice_3(){
+		Notice_2.SetActive (false);
+		Notice_3.SetActive (true);
+		Notice_3_DraggingObject.SetActive (true);
+	}
+
+	void FuncNotice_4(){
+		Notice_3.SetActive (false);
+		Notice_3_DraggingObject.SetActive (false);
+		Notice_4.SetActive (true);
+	}
+}
+
---
+++ Assets/_MyAsset/_Script/DraggingObjects.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggingObjects : MonoBehaviour {
+	string CurrentPimples;
+	GameObject Pimples_1,Pimples_2,Pimples_3,Pimples_4;
+	public static int PimplesCount;
+	bool isPimples_1,isPimples_2,isPimples_3,isPimples_4;
+	// Use this for initialization
+	void Start(){
+		PimplesCount = 0;
+		PimplesCount = 0;
+		isPimples_1 = isPimples_2 = isPimples_3 = isPimples_4 = false;
+		Pimples_1 = GameObject.Find ("Pimples_1");
+		Pimples_1.SetActive (true);
+		Pimples_2 = GameObject.Find ("Pimples_2");
+		Pimples_2.SetActive (true);
+		Pimples_3 = GameObject.Find ("Pimples_3");
+		Pimples_3.SetActive (true);
+		Pimples_4 = GameObject.Find ("Pimples_4");
+		Pimples_4.SetActive (true);
+	}
+	public void Drag () {
+		GameObject.Find ("Notice_3 _DraggingObject").transform.position = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RemovePimple(){
+		if(CurrentPimples == "Pimples_1"){
+			Pimples_1.SetActive (false);
+			Pimples_1.GetComponent<BoxCollider2D> ().enabled = false; 
+			if(isPimples_1 == false){
+				isPimples_1 = true;
+				PimplesCount++;
+			}
+
+		}
+		if(CurrentPimples == "Pimples_2"){
+			Pimples_2.SetActive (false);
+			Pimples_2.GetComponent<BoxCollider2D> ().enabled = false;
+			if(isPimples_2 == false){
+				isPimples_2 = true;
+				PimplesCount++;
+			}
+		}
+		if(CurrentPimples == "Pimples_3"){
+			Pimples_3.SetActive (false);
+			Pimples_3.GetComponent<BoxCollider2D> ().enabled = false;
+			if(isPimples_3 == false){
+				isPimples_3 = true;
+				PimplesCount++;
+			}
+		}
+		if(CurrentPimples == "Pimples_4"){
+			Pimples_4.SetActive (false);
+			Pimples_4.GetComponent<BoxCollider2D> ().enabled = false;
+			if(isPimples_4 == false){
+				isPimples_4 = true;
+				PimplesCount++;
+			}
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.name == "Pimples_1"){
+			CurrentPimples = "Pimples_1";
+		}
+		if(col.name == "Pimples_2"){
+			CurrentPimples = "Pimples_2";
+		}
+		if(col.name == "Pimples_3"){
+			CurrentPimples = "Pimples_3";
+		}
+		if(col.name == "Pimples_4"){
+			CurrentPimples = "Pimples_4";
+		}
+	}
+
+}
